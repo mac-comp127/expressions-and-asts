a2235d025dd72d5b06eac60114a599e07697789a
Paul Cantrell <cantrell@pobox.com>
2020-09-02 22:57:44 -0500

    Created examples for activity

---
+++ src/ExpressionExperiments.java
@@ -0,0 +1,81 @@
+public class ExpressionExperiments {
+    public static void main(String[] args) {
+        System.out.println("–––––– Operator precedence ––––––");
+
+        System.out.print("Example 1: ");
+        System.out.println(3 * 7 + 2);
+
+        // System.out.print("Example 2: ");
+        // System.out.println(3 + 7 * 2);
+
+        // System.out.print("Example 3: ");
+        // System.out.println(8 * 2 + 12 / 4);
+
+        // System.out.print("Example 4: ");
+        // System.out.println(8 * (2 + 12) / 4);
+
+        System.out.println("–––––– Associativity ––––––");
+
+        // System.out.print("Example 5: ");
+        // System.out.println(1000 - 100 - 10 - 1);
+
+        // System.out.print("Example 6: ");
+        // TODO: replace this line with your solution
+
+        // System.out.print("Example 7: ");
+        // System.out.println(__ + __ + " is the sum of our ages");  // TODO: fill in your actual ages
+
+        // System.out.print("Example 8: ");
+        // System.out.println("The sum of our ages is " + __ + __);  // TODO: fill in your actual ages
+
+        // System.out.print("Example 9: ");
+        // TODO: replace this line with your solution
+
+        System.out.println("–––––– Types ––––––");
+
+        // System.out.print("Example 10: ");
+        // System.out.println(1 + 5 / 3);
+
+        // System.out.print("Example 11: ");
+        // System.out.println(1 + 5 / 3.0);
+
+        // System.out.print("Example 12: ");
+        // System.out.println(1 + 5.0 / 3);
+
+        // System.out.print("Example 13: ");
+        // System.out.println(1.0 + 5 / 3);
+
+        // System.out.print("Example 14: ");
+        // int noodle = 5;
+        // System.out.println(1 + noodle / 3);
+
+        // System.out.print("Example 15: ");
+        // double muffin = 5;
+        // System.out.println(1 + muffin / 3);
+
+        System.out.println("–––––– Pre- and Post-increment ––––––");
+
+        // int salad = 0, pretzel = 0;
+
+        // System.out.print("Example 16: ");
+        // pretzel = ++salad;
+        // System.out.println("salad=" + salad + "  pretzel=" + pretzel);
+
+        // System.out.print("Example 17: ");
+        // pretzel = salad++;
+        // System.out.println("salad=" + salad + "  pretzel=" + pretzel);
+
+        // System.out.print("Example 18: ");
+        // pretzel = 7;
+        // pretzel = pretzel++;
+        // System.out.println("pretzel=" + pretzel);
+
+        // System.out.print("Example 19: ");
+        // pretzel = 7;
+        // pretzel = ++pretzel;
+        // System.out.println("pretzel=" + pretzel);
+
+        System.out.println("Done!");
+        System.out.println();
+    }
+}
